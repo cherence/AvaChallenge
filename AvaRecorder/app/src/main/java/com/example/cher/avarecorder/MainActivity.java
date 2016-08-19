745c9d32d33f6f60a6ec5953609307b1eeec33d3
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-08-18 23:47:42 -0700

    Added gitignore

--- AvaRecorder/app/src/main/java/com/example/cher/avarecorder/MainActivity.java
+++ AvaRecorder/app/src/main/java/com/example/cher/avarecorder/MainActivity.java
@@ -9,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
-                AudioFormat.CHANNEL_IN_MONO,
-                AudioFormat.ENCODING_PCM_16BIT);
-
-        AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
-                44100,
-                AudioFormat.CHANNEL_IN_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                bufferSize);
     }
 }
